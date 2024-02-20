3c60204de6ae4a685430d2d5efda0ad42e6ece3f
Tejas Srinivas <109969195+tejas-srinivas@users.noreply.github.com>
2024-02-20 21:26:37 +0530

    Update exhaust.py

--- Raspberry_Pi/exhaust.py
+++ Raspberry_Pi/exhaust.py
@@ -3,15 +3,13 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17,GPIO.OUT)
 GPIO.setwarnings(False)
-GPIO.output(17,GPIO.HIGH)
+
 try:
 	while True:
-		GPIO.output(17,GPIO.HIGH)
 		GPIO.output(17,GPIO.LOW)
 		time.sleep(100000)
 		GPIO.output(17,GPIO.HIGH)
-		time.sleep(7)
-
+		
 except KeyboardInterrupt:
         pass
 
