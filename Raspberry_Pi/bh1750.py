53ceeb153990e60933540242969f3e9fe4af47a6
Tejas Srinivas <109969195+tejas-srinivas@users.noreply.github.com>
2024-02-24 12:46:30 +0530

    Update bh1750.py

--- Raspberry_Pi/bh1750.py
+++ Raspberry_Pi/bh1750.py
@@ -1,30 +1,29 @@
+import time
 import board
 import adafruit_bh1750
 import RPi.GPIO as GPIO
-import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17,GPIO.OUT)
-
+growlight_pin = 19
+GPIO.setup(growlight_pin,GPIO.OUT)
 i2c = board.I2C()
 sensor = adafruit_bh1750.BH1750(i2c)
-grow_light_pin = 9
 
 try:
     while True:
         lux_value = sensor.lux
-        lux = round(lux_value,2)
-        # Check Intensity Condition   
-        if lux < 5000 :
-            print("Light Intensity is Low, Grow Lights is ON")
-            GPIO.output(grow_light_pin,GPIO.LOW)
-        else :
-            print("Light Intensity is Good, Grow Lights is OFF")
-            GPIO.output(grow_light_pin,GPIO.HIGH)
+        lux_value = round(lux_value,2)
+        if lux_value < 5000:
+            GPIO.output(growlight_pin,GPIO.LOW)
+        else:
+            GPIO.output(growlight_pin,GPIO.HIGH)
         print(lux_value)
         time.sleep(10)
+        
 except KeyboardInterrupt:
-    pass
+        pass
 
 finally:
-    GPIO.cleanup()
+    GPIO.cleanup() 
+
+
